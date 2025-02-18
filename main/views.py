52265f86776d63667ab0be3ad55e6652ca395358
deireath <0pospolita0@gmail.com>
2025-02-18 17:52:19 +0700

    fixtures

--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 def index(request):
+    
+    categories = Categories.objects.all()
+
     context = {
         'title': 'Home - Главная',
         'content': 'Магазин мебели HOME',
-        
+        'categories': categories        
     }
     return render(request, 'main/index.html', context)
 
@@ -14,8 +19,8 @@ def about(request):
         'title': 'Home - О нас',
         'content': 'О нас',
         'text_on_page': "Текст о том почему этот магазин такой классный, и какой хороший товар"
-        
     }
+    
     return render(request, 'main/about.html', context)
    
  
