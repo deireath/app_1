b095c78de9a751424f7d2f90ad3446abbb54a187
deireath <0pospolita0@gmail.com>
2025-03-04 19:34:51 +0700

    cart templates

---
+++ carts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def cart_add(reqiest, product_id):
+    ...
+
+def cart_change(reqiest, product_id):
+    ...
+
+def cart_remove(reqiest, product_id):
+    ...
